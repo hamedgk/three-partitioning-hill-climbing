337dc632229da1705d1d8b604e3625badadc4f52
hamed gandomkar <dev.hamedgk@yahoo.com>
2023-12-14 19:33:04 +0330

    add solve modes & remove neighbors count as it's dynamic from now on

--- runner/constants.go
+++ runner/constants.go
@@ -3,11 +3,14 @@ package runner
 type HeritageData = [HeritageDataCount]int
 
 const (
-	NeighborMeetCount = 40
 	HeritageDataCount = 100
 
 	MaxUint = ^uint(0)
 	MinUint = 0
 	MaxInt  = int(MaxUint >> 1)
 	MinInt  = -MaxInt - 1
+	
+	Stochastic = iota
+	Steepest
+	Lazy
 )
