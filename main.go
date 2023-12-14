48f941b9ca162b1537021a99f4abe051d4e08994
hamed gandomkar <dev.hamedgk@yahoo.com>
2023-12-14 19:52:57 +0330

    sort dataset first & use signal handler for print on sigint

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"hill-climbing/runner"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 
-	file, err := os.Open("data/dataset6.txt")
+	file, err := os.Open("data/dataset6-sorted.txt")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		return
@@ -23,12 +25,28 @@ func main() {
 	ss := runner.InitialState(&data, &perfect)
 
 	r := runner.Runner{
-		IterationCount:  10000000,
+		IterationCount:  1000000,
 		Data:            &data,
 		PerfectData:     &perfect,
 		CurrentSequence: ss,
 		BestSequence:    ss,
 	}
 
+	registerSignalHandler(&r.BestSequence)
 	r.Run()
+}
+
+func registerSignalHandler(state **runner.State) {
+	signal_chan := make(chan os.Signal, 1)
+	signal.Notify(signal_chan, syscall.SIGINT)
+
+	go func() {
+		for {
+			s := <-signal_chan
+			if s == syscall.SIGINT {
+				fmt.Printf("%#v", **state)
+				os.Exit(0)
+			}
+		}
+	}()
 }
